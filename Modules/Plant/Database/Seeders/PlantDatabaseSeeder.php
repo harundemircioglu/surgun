808e8f8bff245aa9c275ac8eeb69aa2a3e318f6c
harun demircioğlu <harun.demircioglu02@gmail.com>
2025-06-19 13:44:45 +0300

    plant module

---
+++ Modules/Plant/Database/Seeders/PlantDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Plant\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PlantDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/Plant/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('plant::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('plant.name') !!}</p>
+@endsection
