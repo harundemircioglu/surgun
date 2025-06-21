f117c3110f7056e3a8d34e70a4a8163c413119a7
harun demircioğlu <harun.demircioglu02@gmail.com>
2025-06-21 16:33:07 +0300

    plant module requests update

---
+++ Modules/Plant/App/Http/Requests/Create/PlantMaterialRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Modules\Plant\App\Http\Requests\Create;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PlantMaterialRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
