3ac07db6e9d891cb50d023e2cad2870eb7f920ef
harun demircioğlu <harun.demircioglu02@gmail.com>
2025-06-23 14:40:25 +0300

    seed bank relationships update

--- Modules/Plant/App/Models/SeedBank.php
+++ Modules/Plant/App/Models/SeedBank.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class SeedBank extends Model
 {
     protected $guarded = [];
+
+    public function accessionNotebook()
+    {
+        return $this->belongsTo(AccesionNotebook::class, 'accesion_notebook_id', 'id');
+    }
+
+    public function seedCabinet()
+    {
+        return $this->belongsTo(SeedCabinet::class, 'seed_cabinet_id', 'id');
+    }
 }
