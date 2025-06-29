fbb48eaca4043e03a3f3a7e516f74b3062446174
harun demircioğlu <harun.demircioglu02@gmail.com>
2025-06-29 17:02:27 +0300

    dashboard update

--- Modules/Dashboard/App/Http/Controllers/DashboardController.php
+++ Modules/Dashboard/App/Http/Controllers/DashboardController.php
@@ -6,11 +6,21 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Modules\Auth\App\Models\User;
+use Modules\Plant\App\Models\PlantMaterial;
+use Modules\Plant\App\Models\PlantStatus;
+use Modules\Plant\App\Models\SeedBank;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard::index');
+        $users = User::whereIn('role', [2, 3])->count();
+        $materials = PlantMaterial::where('status', 1)->count();
+        $seedBanks = SeedBank::where('status', 1)->count();
+        $plantStatuses = PlantStatus::where('status', 1)->count();
+
+        return view('dashboard::index', compact('users', 'materials', 'seedBanks', 'plantStatuses'));
     }
 }
