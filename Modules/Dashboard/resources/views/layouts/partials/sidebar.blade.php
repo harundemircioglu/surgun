a8e72801b1b88e12c7e572e0709c4846a4722ecf
harun demircioğlu <harun.demircioglu02@gmail.com>
2025-06-29 17:18:20 +0300

    surgun logo update

--- Modules/Dashboard/resources/views/layouts/partials/sidebar.blade.php
+++ Modules/Dashboard/resources/views/layouts/partials/sidebar.blade.php
@@ -14,7 +14,7 @@
                     </svg>
                 </button>
                 <a href="{{ route('dashboard.index') }}" class="flex ms-2 md:me-24">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
+                    <img src="{{asset('images/surgun_logo.png')}}" class="h-8 me-3" alt="FlowBite Logo" />
                     <span
                         class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Sürgün</span>
                 </a>
